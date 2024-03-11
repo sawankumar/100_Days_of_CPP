cca287e7dab5c4d2f8191d6b248a53ed61075625
sawankumar <33548019+sawankumar@users.noreply.github.com>
2024-03-12 01:36:52 +0530

    Minor Changes

--- Day 15/bubblesort.cpp
+++ Day 15/bubblesort.cpp
@@ -5,31 +5,31 @@ int main()
 {
     int arr[1000];
     int n;
-    cout<<"Enter the size of array: ";
-    cin>>n;
-    cout<<"Enter the array elemnets: ";
-    for(int i=0;i<n;i++)
+    cout << "Enter the size of array: ";
+    cin >> n;
+    cout << "Enter the array elements: ";
+    for(int i = 0; i < n; i++) // Changed i <= n to i < n
     {
-        cin>>arr[i];
+        cin >> arr[i];
     }
 
-    for(int i=n-2;i>=0;i--)
+    for(int i = n - 2; i >= 0; i--)
     {
-        bool swapped = 0;
-        for(int j=0;j<=i;j++)
+        bool swapped = false;
+        for(int j = 0; j <= i; j++)
         {
-            if(arr[j]>arr[j+1])
+            if(arr[j] > arr[j + 1])
             {
-                swap(arr[j], arr[j+1]);
-                swapped = 1;
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
             }
         }
-        if(swapped==0)
+        if(!swapped)
             break;
     }
-    for(int i=0;i<n;i++)
+    for(int i = 0; i < n; i++)
     {
-        cout<<arr[i]<<" ";
+        cout << arr[i] << " ";
     }
     return 0;
 }
